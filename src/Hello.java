b8081a27e9049618a230acdcdd321854a9500f7d
Qiuhq0901 <1005810427@qq.com>
2025-06-18 16:06:24 +0800

    first commit

---
+++ src/Hello.java
@@ -0,0 +1,14 @@
+
+public class Hello {
+
+    private static int num =999;
+
+    public  static  void  main (String[] args)
+    {
+        System.out.println("Hello World" + num);
+
+    }
+
+
+
+}
